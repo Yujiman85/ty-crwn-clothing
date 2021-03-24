f663a66e584e7e559215f6b5cb801f435a8f2a37
Ty Jenkins <cientifik85@gmail.com>
2021-03-24 18:54:18 -0400

    updated shop data with new data, updated cart utils to items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        )
+    }
+    //Return the item with a quantity of 1 if it's not in the cart already
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
